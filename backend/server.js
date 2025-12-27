461a4209246bcdec8a8b47b08ef705e75d2f807e
Alok Ranjan <ar0462287@gmail.com>
2025-12-28 03:04:55 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,11 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // Render ki Public Port ke liye
+    ssl: {
+        rejectUnauthorized: false // Railway Public Connection ke liye zaroori hai
+    }
 });
 
 // Route to serve HTML files and inject partials
